9b7ef3f99383bc53deefb45572788883dff242ac
Luís Sérgio <luissergioaraujo@yahoo.com.br>
2026-01-16 15:17:26 -0300

    Chapter 6

--- proxy.js
+++ proxy.js
@@ -3,9 +3,10 @@ import { get } from '@vercel/edge-config';
 
 export const config = { matcher: '/welcome' };
 
-export async function middleware() {
+export async function proxy() {
   const greeting = await get('greeting');
   // NextResponse.json requires at least Next v13.1 or
   // enabling experimental.allowMiddlewareResponseBody in next.config.js
   return NextResponse.json(greeting);
+  // return NextResponse.json("Bom dia!") - This is a test.
 }
